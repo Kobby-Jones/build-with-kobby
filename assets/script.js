2fe3fb6664ad38676c772a1c233854ff301208e1
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-18 03:05:39 -0800

    Added sample Projects

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(".owl-carousel").owlCarousel({
+  items: 4,
+  loop: true,
+    margin: 10,
+    nav: true,
+  autoplay: true,
+  autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+  dots: true
+});
